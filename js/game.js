0291da26d26bb638af0e4f6825138b84b11b6fb9
Leonardo Cunha <121736215+DevLeoCunha@users.noreply.github.com>
2024-08-31 15:12:19 +0100

    ...

--- js/game.js
+++ js/game.js
@@ -1,5 +1,80 @@
 const grid = document.querySelector('.grid');
 
+const characters = [
+
+    'Albedo',
+    'Alhaitham',
+    'Amber',
+    'Ayaka',
+    'Ayato',
+    'Baizhu',
+    'Barbara',
+    'Beidou',
+    'Bennett',
+    'Candace',
+    'Charlotte',
+    'Chevreuse',
+    'Chongyun',
+    'Collei',
+    'Cyno',
+    'Dehya',
+    'Diluc',
+    'Diona',
+    'Dori',
+    'Eula',
+    'Faruzan',
+    'Fischl',
+    'Freminet',
+    'Furina',
+    'Ganyu',
+    'Gorou',
+    'Hutao',
+    'Itto',
+    'Jean',
+    'Kaeya',
+    'Kaveh',
+    'Kazuha',
+    'Keqing',
+    'Kirara',
+    'Klee',
+    'Kokomi',
+    'Layla',
+    'Lisa',
+    'Lynette',
+    'Lyney',
+    'Mona',
+    'Nahida',
+    'Navia',
+    'Neuvillette',
+    'Nilou',
+    'Ningguang',
+    'Noelle',
+    'Qiqi',
+    'Raiden',
+    'Razor',
+    'Sara',
+    'Sayu',
+    'Shenhe',
+    'Shinobu',
+    'Sucrose',
+    'Tartaglia',
+    'Thoma',
+    'Tighnari',
+    'Venti',
+    'Wanderer',
+    'Wriothesley',
+    'Xiangling',
+    'Xianyun',
+    'Xingqiu',
+    'Yae',
+    'Yanfei',
+    'Yaoyao',
+    'Yelan',
+    'Yoimiya',
+    'Yunjin',
+    'Zhongli',
+];
+
 
 const createElement = (tag, className) => {
     const element = document.createElement(tag);
@@ -20,4 +95,15 @@ const createCard = () => {
 return card;
 }
 
-createCard();
+const loadGame = () => {
+
+    cards.forEach((character) => {
+
+        const card = createCard();
+
+
+    });
+
+}
+
+loadGame();
